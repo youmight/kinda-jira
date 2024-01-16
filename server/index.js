d9a490147ef56ebae0c86de3519f64666230d315
Srajan1 <srakishor.g@gmail.com>
2024-01-16 20:10:44 +0530

    connected to DB

--- server/index.js
+++ server/index.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express();
 const {acceptJson, addRoutes} = require('./utils/utils')
 require('dotenv').config();
-app.listen(process.env.PORT, () => {
+const db = require('./models/index')
+
+
+
+db.sequelize
+  .sync()
+  .then((result) => {
+    app.listen(process.env.PORT, () => {
     acceptJson(app);
     addRoutes(app);
     console.log('App started at port 3000')
 })
+  })
+  .catch((err) => {
+    console.log(err);
+  });
